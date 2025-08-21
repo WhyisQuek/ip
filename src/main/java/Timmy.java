f36e86c2769d93a5aed5bed2bf2b836c0cbeaf80
Timothy <36731402+WhyisQuek@users.noreply.github.com>
2025-08-21 18:26:18 +0800

    Renamed Duke to Timmy, Implemented Greet and Exit

---
+++ src/main/java/Timmy.java
@@ -0,0 +1,12 @@
+public class Timmy {
+    public static void main(String[] args) {
+        String msg = """
+                        ____________________________________________________________
+                        Hello! I'm Timmy
+                        What can I do for you?
+                        ____________________________________________________________
+                        Bye. Hope to see you again soon!
+                        ____________________________________________________________""";
+        System.out.println(msg);
+    }
+}
