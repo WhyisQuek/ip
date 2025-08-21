035e5d0b164b483366d835fc49d970e552581a91
Timothy <36731402+WhyisQuek@users.noreply.github.com>
2025-08-21 18:50:15 +0800

    Added Echo feature

--- src/main/java/Timmy.java
+++ src/main/java/Timmy.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class Timmy {
+    public Timmy(){}
+
     public static void main(String[] args) {
-        String msg = """
+        new Timmy().run();
+    }
+
+    public void run() {
+        Scanner input = new Scanner(System.in);
+        boolean isExit = false;
+        String welcome = """
                         ____________________________________________________________
                         Hello! I'm Timmy
                         What can I do for you?
+                        ____________________________________________________________""";
+        String bye = """
                         ____________________________________________________________
                         Bye. Hope to see you again soon!
                         ____________________________________________________________""";
-        System.out.println(msg);
+        System.out.println(welcome);
+
+        while (!isExit) {
+            String inputString = input.nextLine();
+            if (inputString.equals("bye")) {
+                System.out.println(bye);
+                isExit = true;
+            }
+            else {
+                System.out.println("____________________________________________________________");
+                System.out.println(inputString);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
     }
 }
