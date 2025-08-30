34e0cb8855d6fe16d0c2bec0e1e6bd2c27ac9b36
Timothy Quek <36731402+WhyisQuek@users.noreply.github.com>
2025-08-30 16:23:32 +0800

    Add javadocs

--- src/main/java/Timmy/Deadline.java
+++ src/main/java/Timmy/Deadline.java
@@ -6,11 +6,20 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task with a deadline.
+ */
 public class Deadline extends Task {
     protected final LocalDate end;
     protected final DateTimeFormatter INPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy");
     protected final DateTimeFormatter OUTPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
+    /**
+     * Creates an incomplete task with a specified deadline.
+     *
+     * @param desc  description of the task.
+     * @param end   date of deadline, in d/M/yyyy format.
+     */
     Deadline(String desc, String end) throws TimmyDateParsingException {
         super(desc);
         try {
@@ -20,11 +29,21 @@ public class Deadline extends Task {
         }
     }
 
+    /**
+     * Returns the task as a formatted string for Timmy to output.
+     *
+     * @return task as a formatted string to output on UI.
+     */
     public String toCompleteString() {
         return "[D]" + super.toStringWithStatusIcon()
                 + " (by: " + end.format(OUTPUT_DATE_FORMAT) + ")";
     }
 
+    /**
+     * Returns the task as a formatted string for Timmy to save.
+     *
+     * @return task as a formatted string to save in storage.
+     */
     public String toFileString() {
         return "D | " + (isDone ? "1" : "0") + " | " + super.toString() + " | " + end.format(INPUT_DATE_FORMAT);
     }
